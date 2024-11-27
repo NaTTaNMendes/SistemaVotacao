c91fbe07081ddcb195a5564f8b6a2d056bd16319
NaTTaNMendes <nattan078@gmail.com>
2024-11-27 20:38:32 -0300

    Adicionando a classe pessoa

---
+++ src/modelos/Pessoa.java
@@ -0,0 +1,49 @@
+package modelos;
+
+import java.util.Date;
+
+public abstract class Pessoa {
+	private String nome;
+	private Date dataNascimento;
+	private String tituloEleitor;
+	private String cpf;
+
+	public Pessoa(String nome, Date dataNascimento, String tituloEleitor, String cpf) {
+		this.nome = nome;
+		this.dataNascimento = dataNascimento;
+		this.tituloEleitor = tituloEleitor;
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getTituloEleitor() {
+		return tituloEleitor;
+	}
+
+	public void setTituloEleitor(String tituloEleitor) {
+		this.tituloEleitor = tituloEleitor;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+}
